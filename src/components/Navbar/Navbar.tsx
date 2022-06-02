bc762b584e114b4ab5712a1472614586b96d7172
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-02 21:25:47 +0300

    Add SearchInput component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
+import { Flex, Image } from '@chakra-ui/react'
 import React from 'react'
+
+import RightContent from './RightContent/RightContent'
+import SearchInput from './SearchInput'
 
 const Navbar: React.FC = () => {
     return (
-        <div>Navbar</div>
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex align="center">
+                <Image src="/images/redditFace.svg" height="30px" />
+                <Image src="/images/redditText.svg" height="46px" display={{ base: "none", md: "unset" }} />
+            </Flex>
+            <SearchInput />
+            {/* <Directory /> */}
+            <RightContent />
+        </Flex>
     )
 }
 
