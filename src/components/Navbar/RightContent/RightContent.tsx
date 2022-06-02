5df5e8a11cbe824ce904f4ecf8bb8672ec53dc38
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-02 22:06:49 +0300

    Add AuthModal component

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -2,13 +2,14 @@ import { Flex } from '@chakra-ui/react'
 import React from 'react'
 
 import AuthButtons from './AuthButtons'
+import AuthModal from '../../Modal/Auth/AuthModal'
 
 type RighContentProps = {}
 
 const RightContent: React.FC<RighContentProps> = () => {
     return (
         <>
-            {/* <AuthModel /> */}
+            <AuthModal />
             <Flex justify="center" align="center">
                 <AuthButtons />
             </Flex>
