f705787ddafa445f7e59b7420a742f9cd6aaaadc
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-02 23:11:14 +0300

    fix typo

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -11,7 +11,7 @@ const AuthButtons: React.FC = () => {
     return (
         <>
             <Button variant="outline" height="28px" display={{ base: "none", sm: "flex" }} width={{ base: "70px", md: "110px" }} mr={2} onClick={() => setAuthModalState({ isOpen: true, view: "login" })} >Log In</Button>
-            <Button height="28px" display={{ base: "none", sm: "flex" }} width={{ base: "70px", md: "110px" }} mr={2} onClick={() => setAuthModalState({ isOpen: true, view: "signup" })} >Sign In</Button>
+            <Button height="28px" display={{ base: "none", sm: "flex" }} width={{ base: "70px", md: "110px" }} mr={2} onClick={() => setAuthModalState({ isOpen: true, view: "signup" })} >Sign Up</Button>
         </>
     )
 }
