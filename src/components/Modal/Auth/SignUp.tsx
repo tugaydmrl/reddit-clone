7e2b23d4b205d481c13f2eceaf4d747a1813cc8c
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-03 22:52:51 +0300

    User Creation to firebase

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -1,6 +1,9 @@
 import { Input, Button, Flex, Text } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { useSetRecoilState } from 'recoil'
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+
+import { auth } from "../../../firebase/clientApp"
 import { authModalState } from '../../../atoms/authModalAtom'
 
 type SignUpProps = {}
@@ -11,10 +14,22 @@ const SignUp: React.FC<SignUpProps> = () => {
         email: "",
         password: "",
         confirmPassword: "",
-    })
+    });
+    const [error, setError] = useState("");
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        userError,
+    ] = useCreateUserWithEmailAndPassword(auth);
 
     //firebase
-    const onSubmit = () => { };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (error) setError("");
+        if (signUpForm.password !== signUpForm.confirmPassword) return setError("Passwords do not match");
+        createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
+    };
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         // update form state
@@ -29,7 +44,8 @@ const SignUp: React.FC<SignUpProps> = () => {
             <Input required name="email" placeholder="email" type="email" mb={2} onChange={onChange} fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }} />
             <Input required name="password" placeholder="password" type="password" mb={2} onChange={onChange} fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }} />
             <Input required name="confirmPassword" placeholder="confirm password" type="password" mb={2} onChange={onChange} fontSize="10pt" _placeholder={{ color: "gray.500" }} _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }} _focus={{ outline: "none", bg: "white", border: "1px solid", borderColor: "blue.500" }} />
-            <Button width="100%" height="36px" mt={2} mb={2} type="submit">Sign Up</Button>
+            {error || userError && <Text textAlign="center" color="red" fontSize="10pt">{error || userError.message}</Text>}
+            <Button width="100%" height="36px" mt={2} mb={2} type="submit" isLoading={loading}>Sign Up</Button>
             <Flex fontSize="9pt" justifyContent="center">
                 <Text mr={1}>Already a redditor?</Text>
                 <Text color="blue.500" fontWeight={700} cursor="pointer" onClick={() =>
