3dc4dd7c5996fb8aa8b4e6011925c3cb0dad06ba
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-13 02:29:22 +0300

    Add joinCommunity and leaveCommunity features

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,8 +1,9 @@
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDocs, increment, writeBatch } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useSetRecoilState } from 'recoil'
 
+import { authModalState } from '../atoms/authModalAtom'
 import { Community, CommunitySnippet, communityState } from '../atoms/communitiesAtom'
 import { auth, firestore } from '../firebase/clientApp'
 
@@ -10,12 +11,18 @@ const useCommunityData = () => {
 
     const [user] = useAuthState(auth)
     const [communityStateValue, setCommunityStateValue] = useRecoilState(communityState);
+    const setAuthModalState = useSetRecoilState(authModalState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
     const onJoinOrLeaveCommunity = (communityData: Community, isJoined: boolean) => {
         // user signed in ? if not => open auth modal
+        if (!user) {
+            setAuthModalState({ isOpen: true, view: "login" });
+            return;
+        }
 
+        setLoading(true);
         if (isJoined) {
             leaveCommunity(communityData.id);
             return;
@@ -35,18 +42,71 @@ const useCommunityData = () => {
                 ...prev,
                 mySnippets: snippets as CommunitySnippet[],
             }))
-        } catch (error) {
+        } catch (error: any) {
             console.error("getMySnippets error", error)
+            setError(error.message)
         }
         setLoading(false);
     }
 
-    const joinCommunity = (communityData: Community) => {
+    const joinCommunity = async (communityData: Community) => {
+        // backend updates
 
+        try {
+            const batch = writeBatch(firestore);
+
+            // creating a new community snippet
+            const newSnippet: CommunitySnippet = {
+                communityId: communityData.id,
+                imageURL: communityData.imageURL || "",
+            };
+
+            batch.set(doc(firestore, `users/${user?.uid}/communitySnippets`, communityData.id), newSnippet);
+
+            // updating the numberOfMembers
+            batch.update(doc(firestore, "communities", communityData.id), {
+                numberOfMembers: increment(1)
+            });
+
+            await batch.commit();
+
+            // frontend -> update recoil state - communityState.mySnippets
+            setCommunityStateValue(prev => ({
+                ...prev,
+                mySnippets: [...prev.mySnippets, newSnippet]
+            }))
+        } catch (error: any) {
+            console.error("joinCommunity error", error)
+            setError(error.message);
+        }
+        setLoading(false);
     };
 
-    const leaveCommunity = (communityId: string) => {
+    const leaveCommunity = async (communityId: string) => {
+        // backend site
 
+        try {
+            const batch = writeBatch(firestore);
+            // deleting the community snippet from user
+            batch.delete(doc(firestore, `users/${user?.uid}/communitySnippets`, communityId));
+
+            // updating the numberOfMembers
+            batch.update(doc(firestore, "communities", communityId), {
+                numberOfMembers: increment(-1)
+            });
+
+            await batch.commit();
+            // frontend -> update recoil state - communityState.mySnippets
+            setCommunityStateValue(prev => ({
+                ...prev,
+                mySnippets: prev.mySnippets.filter(item => item.communityId !== communityId)
+            }))
+
+        } catch (error: any) {
+            console.error("leaveCommunity error", error)
+            setError(error.message)
+        }
+        setLoading(false);
     };
 
     useEffect(() => {
