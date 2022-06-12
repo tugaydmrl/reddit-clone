a2552d1e4f91a40376cdfc946102f2263396b639
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-13 02:45:36 +0300

    Fix PageContent layout children

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import React, { ReactNode } from 'react'
 
 interface PageContentProps {
@@ -10,10 +10,9 @@ const PageContent: React.FC<PageContentProps> = ({ children }) => {
         <Flex justify="center" padding="16px 0px">
             <Flex width="95%" justify="center" maxWidth="860px">
                 {/* left side */}
-                <Flex direction="column" width={{ base: "100%", md: "65%" }} mr={{ base: 0, md: 6 }}></Flex>
-
+                <Flex direction="column" width={{ base: "100%", md: "65%" }} mr={{ base: 0, md: 6 }}>{children && children[0 as keyof typeof children]}</Flex>
                 {/* right side */}
-                <Flex direction="column" display={{ base: "none", md: "flex" }} flexGrow={1}></Flex>
+                <Box flexDirection="column" display={{ base: "none", md: "flex" }} flexGrow={1}>{children && children[1 as keyof typeof children]}</Box>
             </Flex>
         </Flex>
     )
