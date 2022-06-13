5e23c8ea8d588f3bb97ed007886703cf9c7b9075
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-13 06:16:01 +0300

    Add back to community page after post success feature

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -88,14 +88,14 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
                     imageURL: downloadURL,
                 });
             }
-
+            
+            // redirect the user back to the communityPage using the router
+            router.back();
         } catch (error: any) {
             console.error("handleCreatePost error", error.message)
             setError(true);
         }
         setLoading(false);
-
-        // redirect the user back to the communityPage using the router
     };
 
     const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
