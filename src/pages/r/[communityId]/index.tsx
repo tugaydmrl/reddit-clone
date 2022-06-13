6d7acf6ecbb5b564be39e95a6e60704e0a599f89
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-13 06:16:42 +0300

    Add usePosts custom hook

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -9,6 +9,7 @@ import NotFound from '../../../components/Community/NotFound';
 import Header from '../../../components/Community/Header';
 import PageContent from '../../../components/Layout/PageContent';
 import CreatePostLink from '../../../components/Community/CreatePostLink';
+import Posts from '../../../components/Posts/Posts';
 
 type CommunityPageProps = {
     communityData: Community;
@@ -24,6 +25,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
             <PageContent>
                 <>
                     <CreatePostLink />
+                    <Posts communityData={communityData} />
                 </>
                 <></>
             </PageContent>
