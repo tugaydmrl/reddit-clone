4a64c784cf76f1dd6e3e61f4528ea3403c9ee0d9
Tugay Demirel <tugaydemirelyedek@gmail.com>
2022-06-16 01:30:26 +0300

    Add access SinglePost page from link

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
+import { Post } from '../../../../atoms/postsAtom'
 import PageContent from '../../../../components/Layout/PageContent'
 import PostItem from '../../../../components/Posts/PostItem'
-import { auth } from '../../../../firebase/clientApp'
+import { auth, firestore } from '../../../../firebase/clientApp'
 import usePosts from '../../../../hooks/usePosts'
 
 const PostPage: React.FC = () => {
     const [user] = useAuthState(auth);
     const { postStateValue, setPostStateValue, onDeletePost, onVote } = usePosts();
+    const router = useRouter();
+
+    const fetchPost = async (postId: string) => {
+        try {
+            const postDocRef = doc(firestore, "posts", postId);
+            const postDoc = await getDoc(postDocRef);
+            setPostStateValue(prev => ({
+                ...prev,
+                selectedPost: {id: postDoc.id, ...postDoc.data() } as Post
+            }))
+        } catch (error: any) {
+            console.error("fetchPost error", error.message)
+        }
+    }
+
+    useEffect(() => {
+        const { pid } = router.query;
+
+        if(pid && !postStateValue.selectedPost){
+            fetchPost(pid as string)
+        }
+    }, [router.query, postStateValue.selectedPost])
 
   return (
     <PageContent>
@@ -16,7 +41,6 @@ const PostPage: React.FC = () => {
             {postStateValue.selectedPost && (
                 <PostItem post={postStateValue.selectedPost} onVote={onVote} onDeletePost={onDeletePost} userVoteValue={postStateValue.postVotes.find(item => item.postId === postStateValue.selectedPost?.id)?.voteValue} userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId} />
             )}
-            {/* <SelectedPost /> */}
             {/* <Comments /> */}
         </>
         <>
